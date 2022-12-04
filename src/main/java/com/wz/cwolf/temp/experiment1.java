10f1ace93fb3d3f1de55ed60b16ed3db82dc8b6e
wangzhe <2546972682@qq.com>
2022-12-04 18:47:14 +0800

    作业提交

--- src/main/java/com/wz/cwolf/temp/experiment1.java
+++ src/main/java/com/wz/cwolf/temp/experiment1.java
@@ -3,26 +3,31 @@ package com.wz.cwolf.temp;
 import java.util.Scanner;
 
 /**
- * @author WangZhe
- * @version 1.0
  * @className experiment1
- * @description
+ * @description 实验1:入门 Java程序编辑、编译、运行、调试，输入，输出，用多种方法求平均值
  * @date 2022/12/2 16:18
  */
 
 
 public class experiment1 {
     public static void main(String[] args) {
-        System.out.println("请输入一组数字回车提交，输入/n 结束输入");
+        //输入多个数字，并求其和与平均值，每输一个数用空格确认
+        System.out.println("输入多个数字，并求其和与平均值，每输一个数用空格确认,输入非数字进行计算");
         Scanner scanner = new Scanner(System.in);
 
-        String tempStr = "/n";
-        do {
-            tempStr = scanner.nextLine();
-            double v = Double.parseDouble(tempStr);
-            System.out.println(v);
-
-        }while (!tempStr.equals("/n"));
+        //和
+        double sum = 0;
+        //计算输了多少个数字
+        int m=0;
+        //通过循环判断是否还有输入，并在里面对每一次进行求和统计
+        while (scanner.hasNextDouble()){
+            double x = scanner.nextDouble();
+            m++;
+            sum=sum+x;
+            System.out.println("你输入了第"+m+"个数据，当前数据的sum="+sum );
+        }
+        System.out.println(m+"个数和为："+sum);
+        System.out.println(m+"个数的平均数为："+(sum/m));
+        scanner.close();
     }
-
 }
